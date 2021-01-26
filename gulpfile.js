0f941e3a0ba4f4442fbc63e0665c0f45bbcdb30c
purelyweb <49037603+purelyweb@users.noreply.github.com>
2021-01-26 21:31:58 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var
+    gulp         = require("gulp"),
+    rename       = require("gulp-rename"),
+    autoprefixer = require("gulp-autoprefixer"),
+    sourcemaps   = require("gulp-sourcemaps"),
+    uglify       = require("gulp-uglify"),
+    browserSync  = require("browser-sync").create(),
+    sass         = require("gulp-sass");
+
+function livereload(done){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 3000
+    });  
+    
+    done();
+}
+
+function browserReload(done){
+    browserSync.reload();
+    done();
+}
+
+function build(done){
+    gulp.src("./scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: "compressed"
+        }))
+        .on("error", console.error.bind(console))
+        .pipe(autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], { cascade: true }))
+        .pipe(rename({suffix: ".min"}))
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("./css/"))
+        .pipe(browserSync.stream());
+
+    done();
+}  
+
+function jsBuild(done){
+	gulp.src('./js/**/main.js')
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(rename({suffix: ".min"}))
+		.pipe(sourcemaps.write("./"))
+		.pipe(gulp.dest("./js/"))
+		.pipe(browserSync.stream());
+
+	done();	
+}
+
+function watch(){
+    gulp.watch("./scss/**/*.scss", build);
+    gulp.watch("./*.html", browserReload);
+    gulp.watch("./js/**/main.js", jsBuild);
+    gulp.watch("php/**/*.php", browserReload);
+}
+
+gulp.task("default", gulp.parallel(watch, livereload));
+gulp.task(livereload);
